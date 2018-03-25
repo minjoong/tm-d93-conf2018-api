efc2290898b4ae69c97e4adef10811d109b22818
Minjoong Kim <minjoong.slow@gmail.com>
2018-03-25 13:30:43 +0900

    MongoDB를 이용한 사용자 관리 API 구현

---
+++ routes/user.js
@@ -0,0 +1,52 @@
+const createError = require('http-errors');
+const express = require('express');
+
+const User = require('../models/user');
+const router = express.Router();
+
+router.get('/:username', (req, res, next) => {
+  User.findOne({username: req.params.username}, function (err, user) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(createError(404));
+    }
+    res.json({user: user});
+  });
+});
+
+router.post('/', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({username: username}, function (err, user) {
+    if (err) {
+      return next(err);
+    }
+    if (user) {
+      return next(createError(409, 'username이 이미 있습니다.'));
+    }
+
+    const newUser = new User({
+      username: username,
+      password: password
+    });
+    newUser.save(next);
+    res.status(201).end();
+  });
+});
+
+router.put('/', (req, res, next) => {
+  req.user.displayName = req.body.displayname;
+  req.user.bio = req.body.bio;
+  req.user.save(function (err) {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json(req.user);
+  });
+});
+
+module.exports = router;
