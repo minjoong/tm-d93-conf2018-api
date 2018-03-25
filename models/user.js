0ffdd515c1dff5e7361665494e1d1207bced628e
Minjoong Kim <minjoong.slow@gmail.com>
2018-03-25 21:05:17 +0900

    username을 email로 변경, 등록 양식 추가

--- models/user.js
+++ models/user.js
@@ -4,11 +4,23 @@ const mongoose = require('mongoose');
 const SALT_FACTOR = 10;
 
 const userSchema = mongoose.Schema({
-  username: {type: String, required: true, unique: true},
+  email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
   createdAt: {type: Date, default: Date.now},
-  displayName: String,
-  bio: String
+  emailConfirmationKey: {type: String, default: () => Math.random().toString(36).substring(2)},
+  isEmailConfirmed: {type: Boolean, default: false},
+  engName: String,
+  korName: String,
+  phone: String,
+  communicationLevel: String,
+  leadershipLevel: String,
+  clubs: [String],
+  roleInClub: String,
+  roleInDistrict: String,
+  conferencePlan: String,
+  depositorName: String,
+  handicapped: String,
+  vipDinner: Boolean
 });
 
 const noop = () => {
@@ -39,10 +51,6 @@ userSchema.methods.checkPassword = function (guess, done) {
   bcrypt.compare(guess, this.password, function (err, isMatch) {
     done(err, isMatch);
   });
-};
-
-userSchema.methods.name = function () {
-  return this.displayName || this.username;
 };
 
 const User = mongoose.model('User', userSchema);
--- routes/user.js
+++ routes/user.js
@@ -4,49 +4,49 @@ const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
 
-router.get('/:username', (req, res, next) => {
-  User.findOne({username: req.params.username}, function (err, user) {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return next(createError(404));
-    }
-    res.json({user: user});
-  });
-});
-
-router.post('/', (req, res, next) => {
-  const username = req.body.username;
+router.post('/', async (req, res, next) => {
+  const email = req.body.email;
   const password = req.body.password;
 
-  User.findOne({username: username}, function (err, user) {
-    if (err) {
-      return next(err);
-    }
+  try {
+    const user = await User.findOne({email: email});
     if (user) {
-      return next(createError(409, 'username이 이미 있습니다.'));
+      return next(createError(409, 'email이 이미 있습니다.'));
     }
 
     const newUser = new User({
-      username: username,
+      email: email,
       password: password
     });
-    newUser.save();
-    res.status(201).end();
-  });
+    await newUser.save();
+
+    return res.status(201).end();
+  } catch (err) {
+    return next(err);
+  }
 });
 
-router.put('/', (req, res, next) => {
+router.get('/:email', async (req, res, next) => {
+  try {
+    const user = await User.findOne({email: req.params.email});
+    if (!user) {
+      return next(createError(404));
+    }
+    return res.json(user);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.put('/', async (req, res, next) => {
   req.user.displayName = req.body.displayname;
   req.user.bio = req.body.bio;
-  req.user.save(function (err) {
-    if (err) {
-      next(err);
-      return;
-    }
-    res.json(req.user);
-  });
+  try {
+    req.user.save();
+    return res.json(req.user);
+  } catch (err) {
+    return next(err);
+  }
 });
 
 module.exports = router;
