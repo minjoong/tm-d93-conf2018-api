2f949d4c1e8791c18da5412b37c7bcbe252b06fa
Minjoong Kim <minjoong.slow@gmail.com>
2018-03-25 23:11:31 +0900

    JWT 토큰 인증 거치면, req.user에 사용자 정보 가져오도록 수정

--- setup-passport.js
+++ setup-passport.js
@@ -2,17 +2,23 @@ const createError = require('http-errors');
 const passport = require('passport');
 const {ExtractJwt, Strategy} = require('passport-jwt');
 
+const User = require('./models/user');
+
 const opts = {};
 
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = 'secret';
 
-passport.use(new Strategy(opts, (payload, done) => {
-  if (payload.email) {
-    return done(null, {
-      email: payload.email
-    });
-  } else {
-    return done(createError(401, 'User not found'), null);
+passport.use(new Strategy(opts, async (payload, done) => {
+  try {
+    const user = await User.findOne({email: payload.email});
+
+    if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  } catch (err) {
+    return done(err, false);
   }
 }));
